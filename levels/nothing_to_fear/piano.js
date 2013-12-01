f3be0f87ba38cca28b4d86e6253a43ae31b29643
Rubén Medellín <chubas7@gmail.com>
2013-12-01 01:40:14 -0600

    Piano section motif

--- levels/nothing_to_fear/piano.js
+++ levels/nothing_to_fear/piano.js
@@ -8,30 +8,41 @@
         var game_h = 500;
 
         var beat = 235; // Distance equals to a beat in the song
+        var chordBeat = 300;
 
         var startX = 1400;
-        var motiff = function(initialX, reverse) {
+        var motiff = function(initialX, index) {
             var construct = [];
             var radius = 40
+            var reverse = index % 2 === 1;
             var angles = reverse ? [180, 135, 90] : [180, 225, 270];
-            var y = reverse ? 20 : game_h - 20;
-            angles.forEach(function(angle, index) {
+            var y = reverse ? 40 : game_h - 40;
+            angles.forEach(function(angle, i) {
                 if(index === 3) {
                     totalEntities.push(
                         ["PRO", 2 - 0.8 * (2 - index), angle,
-                            ["TRI", initialX + 80, y, radius, 0, 1.2, 0]]
+                            ["TRI", initialX + 140, y, radius, angle, 1.2, i]]
 
                     );
                 } else {
                     totalEntities.push(
                         ["PRO", 2 + 0.7 * (2 - index), angle,
-                            ["TRI", initialX, y, radius, 0, 1.2, 0]]
+                            ["TRI", initialX, y, radius, angle, 1.2, i]]
 
                     );
                 }
             });
             return construct;
         };
+
+        var chord = function(initialX) {
+            var construct = [];
+            construct.push(
+                ["PUL", 0.5, 30, 80, 0,
+                    ["CIR", initialX, 250, 30, 1.5, 3]]
+            );
+            return construct;
+        }
 
         
 
@@ -63,7 +74,12 @@
             totalEntities = totalEntities.concat(
                 motiff(
                     startX + (4 * i * beat),
-                    i % 2 === 0
+                    i
+                )
+            );
+            totalEntities = totalEntities.concat(
+                chord(
+                    startX + (4 * i * chordBeat) - 500
                 )
             );
         }
